19819eca91c0498ee46cd69e3f67541063437322
le-genda <le-genda@yandex.ru>
2022-06-01 16:25:32 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles.css';
+
+document.getElementById('app').innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+// Видишь?
+// Да, прям в реале вижу, как ты пишешь :)
+//супер
+// Тут будем короче писать
+console.log('ОКЕЙ:)');
+//Точно
